a83ead1ca11690b755ed17f706e3a8904fc72147
NguyenHongBan <103940809+NguyenHongBan@users.noreply.github.com>
2023-11-12 21:25:45 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBanHang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
