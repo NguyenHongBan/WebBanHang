75d163b8861e4b74802a8da1ae97dd64f1893e68
NguyenHongBan <103940809+NguyenHongBan@users.noreply.github.com>
2023-11-14 09:28:46 +0700

    update 9:28 14/11

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using WebBanHang.Models;
+using WebBanHang.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("QlbanVaLiContext");
+builder.Services.AddDbContext<QlbanVaLiContext>(x => x.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<ILoaiSpRepository, LoaiSpRepository>();
+
+
 
 var app = builder.Build();
 
--- ViewComponents/LoaiSpMenuViewComponent.cs
+++ ViewComponents/LoaiSpMenuViewComponent.cs
@@ -1,9 +1,21 @@
 ﻿using WebBanHang.Models;
 using Microsoft.AspNetCore.Mvc;
+using WebBanHang.Repository;
 
 namespace WebBanHang.ViewComponents
 {
     public class LoaiSpMenuViewComponent : ViewComponent
     {
+        private readonly ILoaiSpRepository _loaiSp;
+
+        public LoaiSpMenuViewComponent(ILoaiSpRepository loaiSpRepository)
+        {
+            _loaiSp = loaiSpRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var loaiSp = _loaiSp.GetAllLoaiSp().OrderBy(x => x.Loai);
+            return View(loaiSp);
+        }
     }
 }
