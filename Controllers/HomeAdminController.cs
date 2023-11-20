8cb62a93423b89bbe082ca86daadfbfcd39a82cf
NguyenHongBan <103940809+NguyenHongBan@users.noreply.github.com>
2023-11-20 23:38:59 +0700

    update 23:38

---
+++ Controllers/HomeAdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBanHang.Controllers
+{
+    
+    [Route("Admin")]
+    [Route("Admin/HomeAdmin")]
+    public class HomeAdminController : Controller
+    {
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
